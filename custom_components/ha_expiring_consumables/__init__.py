9d12a59ff84bb079f5a51deb52889d2d4e74b91c
dfiore1230 <dfiore1230@users.noreply.github.com>
2025-08-26 11:47:03 -0400

    Add config flow and constants

--- custom_components/ha_expiring_consumables/__init__.py
+++ custom_components/ha_expiring_consumables/__init__.py
@@ -1,5 +1,21 @@
-"""HA Expiring Consumables package."""
+"""HA Expiring Consumables integration."""
+
+from .const import DOMAIN
 
 __version__ = "0.1.3"
+
+
+async def async_setup_entry(hass, entry):
+    """Set up HA Expiring Consumables from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {}
+    return True
+
+
+async def async_unload_entry(hass, entry):
+    """Unload a config entry."""
+    hass.data[DOMAIN].pop(entry.entry_id, None)
+    return True
+
 
 __all__ = ["__version__"]
---
+++ custom_components/ha_expiring_consumables/const.py
@@ -0,0 +1,4 @@
+"""Constants for the HA Expiring Consumables integration."""
+
+DOMAIN = "ha_expiring_consumables"
+NAME = "HA Expiring Consumables"
